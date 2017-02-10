8cfdc6d8a784264f61eaac1b2f9f92746183ce09
webbdm <gwebb762@gmail.com>
2017-02-09 20:34:58 -0600

    added alert

--- tree.js
+++ tree.js
@@ -1,15 +1,30 @@
-// Variables & Objects 
+// Variables & Object Function
 
 
 var heightElement = document.getElementById("height");
 var charElement = document.getElementById("character");
 
+var tree = {};
 
-var tree = {
-  
-    height: heightElement.value,
-    character: charElement.value
+
+function treeValues(keypress){
+
+  tree = {height: heightElement.value, character: charElement.value};
+
+    if(keypress.keyCode === 13) { 
+        
+            console.log(tree); 
+
+
+            if (tree.height === "" || tree.character === "") {
+
+            alert("Can't build yo Tree without both parameters");
+
+    }
+  }
 }
+
+
 
 
 // var heightInput = heightElement.value;
@@ -18,16 +33,22 @@ var tree = {
 
 // Functions
 
-function keyEnter(keypress) {
+
+//This works, but is not needed when you just include it after defining the object values in treeValues
+//but it is easily reusable 
+// function keyEnter(keypress) {
   
-  if(keypress.keyCode === 13) { 
+//   if(keypress.keyCode === 13) { 
     
-        console.log();        
-    }
-}
+//         console.log('enter was pressed');        
+//     }
+// }
+
+
+
 
 
 // Listeners
 
-charElement.addEventListener("keypress", keyEnter);
-heightElement.addEventListener("keypress", keyEnter);
+charElement.addEventListener("keypress", treeValues);
+heightElement.addEventListener("keypress", treeValues);
